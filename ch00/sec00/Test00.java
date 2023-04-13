d7eba5a5ede0051c9ce4c08f85646d33d087f691
CGY <CGY@gmail.com>
2023-04-13 11:17:44 +0900

    출력 테스트

---
+++ ch00/sec00/Test00.java
@@ -0,0 +1,11 @@
+package ch00.sec00;
+
+public class Test00 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("끼루루룩");
+		System.out.println("Hello world");
+	}
+
+}
